4774ebc3d8491c8881ea15aca0cf16197f01187f
vctorye <vctorye@gmail.com>
2023-01-25 22:27:30 -0500

    hi

---
+++ main.js
@@ -0,0 +1,81 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  windowOpen("window-child.html","Ratting","width=550,height=170,left=150,top=200,toolbar=0,status=0,")
+  
+
+  // const body = document.querySelector('body');
+  // const widgetContainer = document.createElement('div');
+
+
+  // const widgetContainerStyles = `
+  //   position: absolute;
+  //   bottom: 25px;
+  //   left: 25px;
+  //   margin: 0;
+  //   padding: 0;
+  //   border: 1px solid black;
+  //   height: 50px;
+  //   width: 250px;
+  //   display: grid;
+  //   grid-template-columns: repeat(5, 1fr);
+  //   grid-template-rows: 50px;
+  // `
+
+  // widgetContainer.style = widgetContainerStyles;
+  // body.appendChild(widgetContainer);
+
+  // const widgetStyles = `
+  //   border: 1px solid black;
+  //   border-radius: 10px;
+  //   background-color: tan;
+  //   color: white;
+  //   cursor: pointer;
+  // `;
+
+  // const boomWidget = document.createElement('button');
+  // boomWidget.innerText = "BOOM";
+
+  // const dogWidget = document.createElement('button');
+  // dogWidget.innerText = 'DOG';
+
+  // const oofWidget = document.createElement('button');
+  // oofWidget.innerText = 'OOF';
+  
+  // const bingWidget = document.createElement('button');
+  // bingWidget.innerText = 'bing';
+  
+  // const discordWidget = document.createElement('button');
+  // discordWidget.innerText = 'discord';
+
+
+  // boomWidget.style = widgetStyles;
+  // dogWidget.style = widgetStyles;
+  // oofWidget.style = widgetStyles;
+  // bingWidget.style = widgetStyles;
+  // discordWidget.style = widgetStyles;
+  // widgetContainer.appendChild(boomWidget);
+  // widgetContainer.appendChild(dogWidget);
+  // widgetContainer.appendChild(oofWidget);
+  // widgetContainer.appendChild(bingWidget);
+  // widgetContainer.appendChild(discordWidget);
+
+  // boomWidget.addEventListener('click', () => {
+  //   const boomSound = new Audio('/sounds/vine-boom.mp3');
+  //   boomSound.play();
+  // })
+  
+  // bingWidget.addEventListener('click', () => {
+  //   const bingSound = new Audio('/sounds/bing.mp3');
+  //   bingSound.play();
+  // })
+
+  // oofWidget.addEventListener('click', () => {
+  //   const oofSound = new Audio('./sounds/oof.mp3');
+  //   oofSound.play();
+  // })
+  // discordWidget.addEventListener('click', () => {
+  //   const discordSound = new Audio('/sounds/discord.mp3');
+  //   discordSound.play();
+  // })
+})
